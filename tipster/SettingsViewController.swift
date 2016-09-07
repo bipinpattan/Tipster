e6cbf325ebd3f4b5e63e9881baa70120e45fc9d8
bipin <bipin@crucialbits.com>
2016-09-06 22:03:37 -0700

    Save default tip percentage in the settings.

--- tipster/SettingsViewController.swift
+++ tipster/SettingsViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupData()
+        setupUI()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let savedPercent = loadDefaultTipPercentage()
+        self.pickerView.selectRow(savedPercent, inComponent: 0, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +29,45 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupData() {
+        pickerData = ["18%", "20%", "22%"]
+    }
 
+    func setupUI() {
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+    }
+
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Selection : \(row)")
+        saveDefaultTipPercentage(row)
+    }
+    
+    func saveDefaultTipPercentage(percentage: Int) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(percentage, forKey: "kDefaultTipPercent")
+    }
+
+    func loadDefaultTipPercentage() -> Int {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        return defaults.integerForKey("kDefaultTipPercent")
+    }
+    
     /*
     // MARK: - Navigation
 
